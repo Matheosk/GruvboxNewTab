389769606f82bd3fa99cce4ba52f8c7c5f719d87
Matheo Schonborn Kinderknecht <matheoskinder@gmail.com>
2025-03-11 23:22:27 -0300

    Fix Search

--- sreachScript.js
+++ sreachScript.js
@@ -1,14 +1,15 @@
+let cuadroBusqueda = document.getElementById("textoBusqueda")
+
+cuadroBusqueda.value = ""
+
 function buscarEnDuckDuckGo(event) {
-    // Verifica si la tecla presionada es Enter (código 13)
     if (event.key === "Enter") {
-        // Obtiene el texto del label
-        var textoBusqueda = document.getElementById("textoBusqueda").innerText;
-        // Codifica el texto para ser usado en una URL
-        var url = "https://duckduckgo.com/?q=" + encodeURIComponent(textoBusqueda);
-        // Abre la búsqueda en una nueva pestaña
+        let texto = cuadroBusqueda.value;
+        let url = `https://duckduckgo.com/?q=${encodeURIComponent(texto)}`;
+
         window.open(url, "_blank");
     }
 }
 
 // Asocia el evento al input o al elemento adecuado
-document.getElementById("textoBusqueda").addEventListener("keydown", buscarEnDuckDuckGo);
+cuadroBusqueda.addEventListener("keydown", buscarEnDuckDuckGo);
